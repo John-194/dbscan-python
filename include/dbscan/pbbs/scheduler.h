3ba6ed3d594abfca882ec5b7dd2115704b8a5c1e
Jonas Jankaitis <111707981+John-194@users.noreply.github.com>
2026-09-04 19:39:43 +0300

    cleanup: assert the deque age_t CAS is lock-free at compile time

--- include/dbscan/pbbs/scheduler.h
+++ include/dbscan/pbbs/scheduler.h
@@ -79,6 +79,8 @@ struct Deque {
   // Catches layout issues at compile time on platforms where age_t is not exactly 8 bytes,
   // which would break the lock-free atomic CAS in the work-stealing deque.
   static_assert(sizeof(age_t) == sizeof(int64_t), "age_t must be 8 bytes for atomic CAS");
+  static_assert(std::atomic<age_t>::is_always_lock_free,
+                "age_t CAS must be lock-free, otherwise the deque takes a hidden mutex");
 
   // align to avoid false sharing
   struct alignas(64) padded_job {
